5d6c9be967484f2b279ebfa5750b7424141f141c
ibrahemomari <ibrahem.omari96@gmail.com>
2022-03-10 21:30:51 +0300

    phase5

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Container, Row } from "react-bootstrap";
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import "./footer.scss";
 import {
   AiFillFacebook,
@@ -7,40 +7,86 @@ import {
   AiFillTwitterCircle,
   AiFillGithub,
 } from "react-icons/ai";
+import { Parallax } from "react-scroll-parallax";
 function Footer() {
   return (
     <>
-      <section className="footer">
-        <Container>
-          <Row>
-            <div className="social-container">
-              <h3 className="list-title">Follow Me :</h3>
-              <ul>
-                <li>
-                  <a href="">
-                    <AiFillGithub />
-                  </a>
-                </li>
-                <li>
-                  <a href="">
-                    <AiFillFacebook />
-                  </a>
-                </li>
-                <li>
-                  <a href="">
-                    <AiFillInstagram />
-                  </a>
-                </li>
-                <li>
-                  <a href="">
-                    <AiFillTwitterCircle />
-                  </a>
-                </li>
+      <Parallax translateX={[-100, 100]}>
+        <section className="footer">
+          <Container>
+            <Row>
+              <Col xl={6}>
+                <div className="social-container">
+                  <h3 className="list-title">Follow Me : </h3>
+                  <ul>
+                    <li>
+                      <a href="">
+                        <AiFillGithub />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <AiFillFacebook />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <AiFillInstagram />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        <AiFillTwitterCircle />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+              <Col xl={6}></Col>
+            </Row>
+            <Row></Row>
+          </Container>
+          <div className="rocket-div">
+            <div class="rocket">
+              <div class="rocket-body">
+                <div class="body"></div>
+                <div class="fin fin-left"></div>
+                <div class="fin fin-right"></div>
+                <div class="window"></div>
+              </div>
+              <div class="exhaust-flame"></div>
+              <ul class="exhaust-fumes">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              <ul class="star">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
               </ul>
             </div>
-          </Row>
-        </Container>
-      </section>
+          </div>
+          <div className="footer-name">
+            <Parallax translateY={[80, -50]} translateX={[100, -100]}>
+              <h2>
+                ibrahem <br />
+                alomari
+              </h2>
+            </Parallax>
+          </div>
+        </section>
+      </Parallax>
     </>
   );
 }
